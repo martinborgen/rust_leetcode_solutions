6069f1125a7107dbc943cf7d9344b5141b9123a7
Martin Borgén <martinb.nymail@gmail.com>
2023-06-01 01:28:49 +0200

    WIP boyer-moore

--- lc28_first_occ_in_str/src/main.rs
+++ lc28_first_occ_in_str/src/main.rs
@@ -23,22 +23,45 @@ Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 Constraints:
 
-    1 <= haystack.length, needle.length <= 104
+    1 <= haystack.length, needle.length <= 10^4
     haystack and needle consist of only lowercase English characters.
 */
 // use num_traits::cast::AsPrimitive;
+// use std::collections::HashMap;
+use std::collections::HashSet;
 
 struct Solution{}
 
 impl Solution {
     pub fn str_str(haystack: String, needle: String) -> i32 {
         // Idea is to use Boyer-Moore 
-        let needle_it = needle.chars();
-        let haystack_it = haystack.chars();
-        let needle_len = needle.chars().count();
-        let tst: i32 = i32::try_from(needle_len).unwrap();
-        return tst;
+        let needle_vec: Vec<char> = needle.chars().collect();
+        let haystack_vec: Vec<char> = haystack.chars().collect();
+        let needle_len: usize = needle.chars().count();
+        let shift: usize = 0;
+        let i: usize = 0;
+
+        // Hashing in the needle with it's indices- SKIP FOR NOW; COLLISIONS MAKES IT HARDER
+        // let mut needle_hash: HashMap<usize, char> = HashMap::new();
+
+        let mut needle_hash = HashSet::new();
+        for c in needle.chars() {
+            needle_hash.insert(c);
+        }
+
+        while true {
+            let haystack_char: char = haystack_vec[needle_len];
+            if haystack_char != needle_vec[needle_vec.len() -1] {
+                if needle_hash.contains(&haystack_char) {
+                    
+                }
+            }
+            
+        }
+
+        return 0;
     }
+
 }
 
 fn main() {
