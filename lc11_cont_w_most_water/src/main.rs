cc6637cf3c81e90aa3f1081d0d8199c2dc977cc0
Martin Borgén <martinb.nymail@gmail.com>
2023-06-01 19:35:44 +0200

    initial working (unoptimized) version

---
+++ lc11_cont_w_most_water/src/main.rs
@@ -0,0 +1,33 @@
+// Container with most water
+// Martin Borgén
+// 2023-06-02
+
+use std::cmp::min;
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut largest = 0;
+
+        for i in 0..height.len() {
+            let hi = height[i];
+
+            for j in (i..height.len()).rev() {
+                let hj = height[j];
+
+                let area = min(hi, hj) * (j - i) as i32;
+                if area > largest {
+                    largest = area;
+                }
+            }
+        }
+        return largest;
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::max_area(vec![1,8,6,2,5,4,8,3,7]), 49);
+    assert_eq!(Solution::max_area(vec![1,1]), 1);
+    assert_eq!(Solution::max_area(vec![10000; 100000]), 1000000000);
+}
