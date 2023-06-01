ef0c4004f96fcaae10ba00b1c0d01cb910ba8374
Martin Borgén <martinb.nymail@gmail.com>
2023-06-01 09:02:01 +0200

    initial lc1091 commit

---
+++ lc1091_shortes_path_in_bin_matrix/src/main.rs
@@ -0,0 +1,23 @@
+
+struct Cell {
+    row: i32,
+    col: i32,
+    val: i32,
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn shortest_path_binary_matrix(grid: Vec<Vec<i32>>) -> i32 {
+        
+    }
+
+    /// Returns the adjecant cells.
+    fn get_adjecants(grid: Vec<Vec<i32>>, origin) -> Vec<i32> {
+        
+    }
+}
+
+fn main() {
+    res = Solution::shortest_path_binary_matrix();
+}
