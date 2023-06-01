696558279fab632a368e8fda0bf01258018f2bde
Martin Borgén <martinb.nymail@gmail.com>
2023-06-01 21:04:16 +0200

    implemented my own version of the common 'good' solution

--- lc11_cont_w_most_water/src/main.rs
+++ lc11_cont_w_most_water/src/main.rs
@@ -7,33 +7,26 @@ use std::cmp::min;
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let mut largest = 0;
-        let mut hi_max = 0;
-
-        for i in 0..height.len() {
-            let hi = height[i];
-            if hi > hi_max {
-                hi_max = hi;
-            } else {
-                continue;
+        let mut left: usize = 0;
+        let mut right = height.len() - 1;
+        
+        while right != left {
+            let hl = height[left];
+            let hr = height[right];
+            let area = min(hr, hl) * (right - left) as i32;
+            if area > largest {
+                largest = area;
             }
-
-            for j in (i..height.len()).rev() {
-                let hj = height[j];
-
-                let area = min(hi, hj) * (j - i) as i32;
-                if area > largest {
-                    largest = area;
-                }
-
-                if hj >= hi {
-                    break;
-                }
+            
+            if hl < hr {
+                left += 1;
+            } else {
+                right -= 1;
             }
         }
         return largest;
     }
 }
-
 struct Solution;
 
 fn main() {
@@ -42,23 +35,5 @@ fn main() {
     assert_eq!(Solution::max_area(vec![10000; 100000]), 999990000);
     assert_eq!(Solution::max_area(vec![1,832,6,2,5,4,8,3,7,1,2,4,4,6,456,23,76,3,32,23,4,5,5,64,546,7,2,345,123,74,432]), 12558);
     assert_eq!(Solution::max_area(vec![1,832,6,2,5,4,8,3,10000,1,2,4,4,6,456,23,76,3,32,23,4,5,5,64,546,7,2,345,123,74,432,1,2,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,10000,3]), 600000);
+    assert_eq!(Solution::max_area(vec![1,10,1,2,3,4,3,2,1,300,10,2]), 90);
 }
-
-// Better slution idea here:
-// impl Solution {
-//     pub fn max_area(height: Vec<i32>) -> i32 {
-//         let (mut start, mut end) = (0, height.len() - 1);
-//         let mut biggest_area = 0;
-//         while start != end {
-//             let area = (end - start) as i32 * i32::min(height[start], height[end]);
-//             if height[start] > height[end] {
-//                 end -= 1;
-//             }
-//             else {
-//                 start += 1;
-//             }
-//             biggest_area = i32::max(area, biggest_area)
-//         }
-//         biggest_area
-//     }
-// }
