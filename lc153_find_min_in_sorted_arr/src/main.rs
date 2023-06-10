f924f7caa9b5640b6a79fa32947e8a2a29306f6e
Martin Borgén <martinb.nymail@gmail.com>
2023-06-10 03:36:44 +0200

    more elegant code, doubt if performing better.

--- lc153_find_min_in_sorted_arr/src/main.rs
+++ lc153_find_min_in_sorted_arr/src/main.rs
@@ -1,36 +1,24 @@
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
         let mut left: usize = 0;
-        let mut mid: usize = nums.len() / 2;
         let mut right: usize = nums.len() - 1;
-
+        let mut mid: usize;
         if nums[left] < nums [right] {
             // means no shift has occurred
             return nums[left];
         }
         
-        loop {
-            // first checking if we've found a minimum
-            if mid == right {
-                return nums[mid];
-            } else if mid == left {
-                return nums [right];
-            }
-            
-            // deciding where to go
+        while left < right {
+            mid = (right + left) / 2;
             if nums[mid] > nums [right] {
                 // means shift is in right half
-                left = mid;
-                mid = mid + (right - mid) / 2;
-                // continue;
+                left = mid + 1;
             } else {
                 // means hsift is in left half
                 right = mid;
-                mid = mid - (mid - left) / 2;
-                // continue;
             }
-
         }
+        return nums[right];
     }
 }
 
@@ -43,4 +31,5 @@ fn main() {
     assert_eq!(Solution::find_min(vec![11,13,15,17]), 11);
     assert_eq!(Solution::find_min(vec![4,5,0,1,2,3]), 0);
     assert_eq!(Solution::find_min(vec![4,5,6,7,2,3]), 2);
+    assert_eq!(Solution::find_min(vec![3,1,2]), 1);
 }
