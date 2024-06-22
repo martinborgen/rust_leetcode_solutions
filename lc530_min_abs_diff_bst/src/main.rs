854d4ef1a345f6b00f99add59326fe2aa2beeb5e
Martin Borgén <martinb.nymail@gmail.com>
2024-06-22 18:42:36 +0200

    implemented intended algorithm, turns out algorithm does not solve the problem

--- lc530_min_abs_diff_bst/src/main.rs
+++ lc530_min_abs_diff_bst/src/main.rs
@@ -23,23 +23,46 @@ impl TreeNode {
 }
 
 use std::cell::RefCell;
+use std::cmp::min;
 use std::collections::VecDeque;
 use std::rc::Rc;
 impl Solution {
     pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let left_res;
-        let right_res;
+        let left;
+        let right;
+        let left_res: i32;
+        let right_res: i32;
         let diff_left_right: i32;
         let diff_root_left: i32;
         let diff_root_right: i32;
         match root {
             Some(root) => {
-                left_res = Self::get_minimum_difference(root.borrow().left.clone());
-                right_res = Self::get_minimum_difference(root.borrow().right.clone());
-                if left_res < right_res {
-                    return left_res;
-                } else {
-                    return right_res;
+                left = root.borrow().left.clone();
+                right = root.borrow().right.clone();
+                match (left, right) {
+                    (Some(left), Some(right)) => {
+                        left_res = i32::abs(Self::get_minimum_difference(Some(left.clone())));
+                        right_res = i32::abs(Self::get_minimum_difference(Some(right.clone())));
+                        diff_left_right = i32::abs(left.borrow().val - right.borrow().val);
+                        diff_root_left = i32::abs(left.borrow().val - root.borrow().val);
+                        diff_root_right = i32::abs(right.borrow().val - root.borrow().val);
+                        return i32::abs(min(
+                            diff_left_right,
+                            min(
+                                min(left_res, right_res),
+                                min(diff_root_left, diff_root_right),
+                            ),
+                        ));
+                    }
+                    (None, None) => return i32::MAX,
+                    (None, Some(right)) => {
+                        right_res = i32::abs(Self::get_minimum_difference(Some(right.clone())));
+                        return min(i32::abs(right.borrow().val - root.borrow().val), right_res);
+                    }
+                    (Some(left), None) => {
+                        left_res = i32::abs(Self::get_minimum_difference(Some(left.clone())));
+                        return min(i32::abs(left.borrow().val - root.borrow().val), left_res);
+                    }
                 }
             }
             None => i32::MAX,
@@ -97,10 +120,33 @@ fn make_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
 }
 
 fn main() {
-    let test_vec: Vec<Option<i32>> =
+    let test_vec1: Vec<Option<i32>> =
         vec![Some(1), Some(0), Some(48), None, None, Some(12), Some(49)];
-    let tree = make_tree(test_vec);
+    let tree1 = make_tree(test_vec1);
 
-    let res = Solution::get_minimum_difference(tree);
-    println!("{res}");
+    let res1 = Solution::get_minimum_difference(tree1);
+    assert_eq!(res1, 1);
+
+    let test_vec2: Vec<Option<i32>> = vec![Some(4), Some(2), Some(6), Some(1), Some(3)];
+    let tree2 = make_tree(test_vec2);
+    let res2 = Solution::get_minimum_difference(tree2);
+    assert_eq!(res2, 1);
+
+    let test_vec3: Vec<Option<i32>> = vec![Some(1), None, Some(3), Some(2)];
+    let tree3 = make_tree(test_vec3);
+    let res3 = Solution::get_minimum_difference(tree3);
+    assert_eq!(res3, 1);
+
+    let test_vec4: Vec<Option<i32>> = vec![
+        Some(236),
+        Some(104),
+        Some(701),
+        None,
+        Some(227),
+        None,
+        Some(911),
+    ];
+    let tree4 = make_tree(test_vec4);
+    let res4 = Solution::get_minimum_difference(tree4);
+    assert_eq!(res4, 9);
 }
