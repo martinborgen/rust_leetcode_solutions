66c79dfc5c7305f19bda6476db64d0bc5ed43be6
Martin Borgén <martinb.nymail@gmail.com>
2024-06-22 17:38:03 +0200

    started rework, using tree making function from lc100 solution

--- lc530_min_abs_diff_bst/src/main.rs
+++ lc530_min_abs_diff_bst/src/main.rs
@@ -6,69 +6,101 @@
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
 
-
-
-use std::rc::Rc;
 use std::cell::RefCell;
-// impl Solution {
-//     pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        
-//     }
-// }
-
+use std::collections::VecDeque;
+use std::rc::Rc;
+impl Solution {
+    pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let left_res;
+        let right_res;
+        let diff_left_right: i32;
+        let diff_root_left: i32;
+        let diff_root_right: i32;
+        match root {
+            Some(root) => {
+                left_res = Self::get_minimum_difference(root.borrow().left.clone());
+                right_res = Self::get_minimum_difference(root.borrow().right.clone());
+                if left_res < right_res {
+                    return left_res;
+                } else {
+                    return right_res;
+                }
+            }
+            None => i32::MAX,
+        }
+    }
+}
 
 struct Solution;
 
-fn tree_init_from_vec(data: Vec<Option<i32>>) -> Rc<RefCell<TreeNode>> {
-    let mut root = Rc::new(RefCell::new(TreeNode::new(data[0].unwrap())));
-    for i in &data[1..] {
-        match i {
-            Some(n) => root = tree_push(&Some(root.clone()), *n),
-            None => continue
-        };
+fn make_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if vec.is_empty() || vec[0].is_none() {
+        return None;
     }
-    return root;
-}
+    let root: Option<Rc<RefCell<TreeNode>>> = Some(Rc::new(RefCell::new(TreeNode {
+        val: vec[0].unwrap(),
+        left: None,
+        right: None,
+    })));
+    let mut output: Option<Rc<RefCell<TreeNode>>> = None; // This is just to re-catch the root to circumvent the damn borrow checker
+    let mut stack: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    stack.push_back(root);
 
-fn tree_push(root_opt: &Option<Rc<RefCell<TreeNode>>>, data: i32) -> Rc<RefCell<TreeNode>> {
-    let root: Rc<RefCell<TreeNode>>;
-    match root_opt {
-        Some(ref x) => root = x.to_owned(),
-        None => return Rc::new(RefCell::new(TreeNode::new(data))),
-    }
-    
-    let root_val = root.try_borrow().unwrap().val; // we know root exists now, so it must have a val
-    if data == root_val {
-        return root_opt.clone().unwrap();
-    } else if data < root_val {
-        let tmp = tree_push(&root.borrow().left, data);
-        root.borrow_mut().left = Some(tmp);
-    } else {
-        let tmp = tree_push(&root.borrow().right, data);
-        root.borrow_mut().right = Some(tmp);
-    }
+    let mut i = 1;
+    while i < vec.len() {
+        let popped = stack.pop_front();
+        if let Some(Some(current)) = popped {
+            if let Some(x) = vec[i] {
+                current.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode {
+                    val: x,
+                    left: None,
+                    right: None,
+                })));
+                stack.push_back(current.borrow_mut().left.clone());
+            }
 
-    return root;
+            if i + 1 < vec.len() {
+                if let Some(y) = vec[i + 1] {
+                    current.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode {
+                        val: y,
+                        left: None,
+                        right: None,
+                    })));
+                    stack.push_back(current.borrow_mut().right.clone());
+                }
+            }
+
+            if i == 1 {
+                output = Some(current); // explicitly handing back root. Gotta be a better way?
+            }
+
+            i += 2;
+        }
+    }
+    output
 }
 
 fn main() {
-    let test_vec: Vec<Option<i32>> = vec![Some(1),Some(0),Some(48),None,None,Some(12),Some(49)];
-    let tree = tree_init_from_vec(test_vec);
-    println!("hello kek");
+    let test_vec: Vec<Option<i32>> =
+        vec![Some(1), Some(0), Some(48), None, None, Some(12), Some(49)];
+    let tree = make_tree(test_vec);
+
+    let res = Solution::get_minimum_difference(tree);
+    println!("{res}");
 }
