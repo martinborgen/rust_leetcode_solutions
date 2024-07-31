2f5af687a0a26fc05b0dac24d4a5d2e6150aa090
Martin Borgén <martinb.nymail@gmail.com>
2024-07-31 16:22:23 +0200

    Previous attempt in C, now attempting in rust

---
+++ lc29_divide_two_integers/src/main.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn divide(dividend: i32, divisor: i32) -> i32 {
+        if i32::abs(divisor) == 1 {
+            todo();
+        }
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!("Hello, world!");
+}
