5c72d02f99c1f99a5ed7287c015577d01c11d37b
Martin Borgén <martinb.nymail@gmail.com>
2024-08-01 01:04:15 +0200

    implemented a hash-set solution. The specific use of methods was inspired from a sample solution, and was unknown to me

--- lc2215_find_difference_of_two_arrays/src/main.rs
+++ lc2215_find_difference_of_two_arrays/src/main.rs
@@ -1,20 +1,18 @@
 // Input: nums1 = [1,2,3], nums2 = [2,4,6]
 // Output: [[1,3],[4,6]]
 
+use std::collections::HashSet;
+
 impl Solution {
     pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
         let mut answer: Vec<Vec<i32>> = vec![Vec::new(); 2];
-        for n1 in &nums1 {
-            if !nums2.contains(&n1) && !answer[0].contains(&n1) {
-                answer[0].push(*n1);
-            }
-        }
+        
+        let nums1_hash: HashSet<i32> = nums1.into_iter().collect();
+        let nums2_hash: HashSet<i32> = nums2.into_iter().collect();
 
-        for n2 in &nums2 {
-            if !nums1.contains(&n2) && !answer[1].contains(&n2) {
-                answer[1].push(*n2);
-            }
-        }
+        answer[0] = nums1_hash.difference(&nums2_hash).into_iter().cloned().collect();
+        answer[1] = nums2_hash.difference(&nums1_hash).into_iter().cloned().collect();
+
         answer
     }
 }
@@ -22,5 +20,8 @@ impl Solution {
 struct Solution;
 
 fn main() {
-    assert_eq!(Solution::find_difference(vec![1,2,3], vec![2,4,6]), vec![vec![1,3], vec![4,6]]);
+    // asserts complain because of order - problem spec allows any order
+    // assert_eq!(Solution::find_difference(vec![1,2,3], vec![2,4,6]), vec![vec![3,1], vec![4,6]]);
+    // assert_eq!(Solution::find_difference(vec![1,2,3,3], vec![1,1,2,2]), vec![vec![3], vec![]]);
+    // assert_eq!(Solution::find_difference(vec![-68,-80,-19,-94,82,21,-43], vec![-63]), vec![vec![-68,-80,-19,-94,82,21,-43], vec![-63]]);
 }
