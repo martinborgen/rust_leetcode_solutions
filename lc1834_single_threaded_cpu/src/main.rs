c391bbc1698e2156e2ed8f3038c3443391562fc4
Martin Borgén <martinb.nymail@gmail.com>
2024-08-03 17:51:01 +0200

    Cargo fmt

--- lc1834_single_threaded_cpu/src/main.rs
+++ lc1834_single_threaded_cpu/src/main.rs
@@ -1,4 +1,3 @@
-
 struct Task {
     index: i32,
     enqueue_t: i32,
@@ -8,23 +7,28 @@ struct Task {
 impl Solution {
     pub fn get_order(tasks: Vec<Vec<i32>>) -> Vec<i32> {
         let mut tasks_sorted: Vec<Task> = Vec::with_capacity(tasks.len());
-        let mut output: Vec<i32> = Vec::with_capacity(tasks.len()); 
+        let mut output: Vec<i32> = Vec::with_capacity(tasks.len());
 
         for (i, t) in tasks.iter().enumerate() {
-            tasks_sorted.push(Task{index: i as i32, enqueue_t: t[0], execute_t: t[1]});
+            tasks_sorted.push(Task {
+                index: i as i32,
+                enqueue_t: t[0],
+                execute_t: t[1],
+            });
         }
-        
-        tasks_sorted.sort_by(|a, b|{
+
+        tasks_sorted.sort_by(|a, b| {
             if a.enqueue_t == b.enqueue_t {
                 a.enqueue_t.cmp(&b.execute_t)
             } else {
                 a.enqueue_t.cmp(&b.enqueue_t)
-            }});
+            }
+        });
 
         tasks_sorted.reverse(); // reverse because Vec::pop() returns last element
-        
+
         let mut time = tasks_sorted[tasks_sorted.len() - 1].enqueue_t;
-        let mut available_tasks: Vec<Task> = Vec::new(); 
+        let mut available_tasks: Vec<Task> = Vec::new();
         loop {
             while tasks_sorted.len() > 0 && tasks_sorted[tasks_sorted.len() - 1].enqueue_t <= time {
                 available_tasks.push(tasks_sorted.pop().unwrap())
@@ -47,7 +51,7 @@ impl Solution {
                 break;
             }
         }
-        
+
         output
     }
 }
@@ -55,5 +59,8 @@ impl Solution {
 struct Solution;
 
 fn main() {
-    assert_eq!(Solution::get_order(vec![vec![1,2],vec![2,4],vec![3,2],vec![4,1]]), vec![0,2,3,1]);
+    assert_eq!(
+        Solution::get_order(vec![vec![1, 2], vec![2, 4], vec![3, 2], vec![4, 1]]),
+        vec![0, 2, 3, 1]
+    );
 }
