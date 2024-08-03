59178fe604ea102472dad7499498c7db38e85c37
Martin Borgén <martinb.nymail@gmail.com>
2024-08-03 18:11:20 +0200

    fixed bug where tasks of the same execution time weren't processed in the correct order

--- lc1834_single_threaded_cpu/src/main.rs
+++ lc1834_single_threaded_cpu/src/main.rs
@@ -30,14 +30,17 @@ impl Solution {
         let mut time = tasks_sorted[tasks_sorted.len() - 1].enqueue_t;
         let mut available_tasks: Vec<Task> = Vec::new();
         loop {
-            while !tasks_sorted.is_empty() && tasks_sorted[tasks_sorted.len() - 1].enqueue_t <= time {
+            while !tasks_sorted.is_empty() && tasks_sorted[tasks_sorted.len() - 1].enqueue_t <= time
+            {
                 available_tasks.push(tasks_sorted.pop().unwrap())
             }
 
             let mut min_idx: usize = 0;
             let mut min = i32::MAX;
             for (i, t) in available_tasks.iter().enumerate() {
-                if t.execute_t < min {
+                if t.execute_t < min
+                    || (t.execute_t == min && t.index < available_tasks[min_idx].index)
+                {
                     min_idx = i;
                     min = t.execute_t;
                 }
@@ -59,8 +62,39 @@ impl Solution {
 struct Solution;
 
 fn main() {
+    // assert_eq!(
+    //     Solution::get_order(vec![vec![1, 2], vec![2, 4], vec![3, 2], vec![4, 1]]),
+    //     vec![0, 2, 3, 1]
+    // );
+
+    // assert_eq!(
+    //     Solution::get_order(vec![
+    //         vec![7, 10],
+    //         vec![7, 12],
+    //         vec![7, 5],
+    //         vec![7, 4],
+    //         vec![7, 2]
+    //     ]),
+    //     vec![4, 3, 2, 0, 1]
+    // );
+
     assert_eq!(
-        Solution::get_order(vec![vec![1, 2], vec![2, 4], vec![3, 2], vec![4, 1]]),
-        vec![0, 2, 3, 1]
+        Solution::get_order(vec![
+            vec![19, 13],
+            vec![16, 9],
+            vec![21, 10],
+            vec![32, 25],
+            vec![37, 4],
+            vec![49, 24],
+            vec![2, 15],
+            vec![38, 41],
+            vec![37, 34],
+            vec![33, 6],
+            vec![45, 4],
+            vec![18, 18],
+            vec![46, 39],
+            vec![12, 24]
+        ]),
+        vec![6, 1, 2, 9, 4, 10, 0, 11, 5, 13, 3, 8, 12, 7]
     );
 }
