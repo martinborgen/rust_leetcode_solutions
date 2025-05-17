c03ab8b2fd5356ca9b2a8d0eb1015d2ac0a3bab6
Martin Borgén <martinb.nymail@gmail.com>
2025-05-17 22:54:46 +0200

    complete re-work as I'm pretty sure I did not correctly understand the problem. This is a naive solution, will have to be re-written to a more performant implementation.

--- lc2719_count_of_integers/src/main.rs
+++ lc2719_count_of_integers/src/main.rs
@@ -1,168 +1,69 @@
-// LeetCode problem 2719
-// Martin Borgén
-// 2023-06-04
+/* You are given two numeric strings num1 and num2 and two integers max_sum and min_sum. We denote an integer x to be good if:
 
-impl Solution {
-    pub fn count(num1: String, num2: String, min_sum: i32, max_sum: i32) -> i32 {
-        let base: i32 = 10;
-        let mod_fact = base.pow(9) + 7;
+    num1 <= x <= num2
+    min_sum <= digit_sum(x) <= max_sum.
 
-        // some obervations: the max digit sum for a 10**22 integer is 9*22 = 198.
-        let n1: i128 = i128::from_str_radix(&num1, 10).unwrap();
-        let n2: i128 = i128::from_str_radix(&num2, 10).unwrap();
-        let mut count = 0;
-        return count % (base.pow(9) + 7);
-    }
-    
-    // num1 is start, num2 is stop. All vectors should have the same length 
-    fn count_fun(num1: Vec<i32>, num2: Vec<i32>, min_sum: Vec<i32>, max_sum: Vec<i32>) -> i32 {
-        let mut count = 0;
-        let mut old = 1;
-        for i in 0..num2.len() {
-            let digit_range = num2[i] - num1[i];
-            count *= digit_range.abs();
-        }
-        return count;
-    }
+Return the number of good integers. Since the answer may be large, return it modulo 109 + 7.
 
-    fn digit_sum(num: i128) -> i32 {
-        let mut sum: i128 = 0;
-        let mut working = num;
-        while working > 0 {
-            sum += working % 10;
-            working /= 10;
-            // if sum > max as i128 {
-            //     return -1;
-            // }
-        }
-        return sum as i32;
-    }
+Note that digit_sum(x) denotes the sum of the digits of x.
 
-    fn digit_sum_str(num: &String, max: i32) -> String {
-        let mut sum = 0;
 
-        let mut max_digits = 0;
-        let mut max_tmp = max;
-        while max > 0 {
-            max_tmp /= 10;
-            max_digits += 1;
-        }
 
-        if num.len() as i32 * 9 < max {
-            return num.clone();
-        }
+Example 1:
 
-        for c in num.chars() {
-            sum += c as i32 - 48;
-            if sum >= max {
-                return "-1".to_string();
-            }
-        }
-        return sum;
-    }
-}
+Input: num1 = "1", num2 = "12", min_sum = 1, max_sum = 8
+Output: 11
+Explanation: There are 11 integers whose sum of digits lies between 1 and 8 are 1,2,3,4,5,6,7,8,10,11, and 12. Thus, we return 11.
 
-fn digit_sum(num: i32) -> i32 {
-    let mut sum = 0;
-    let mut working = num;
-    while working > 0 {
-        sum += working % 10;
-        working /= 10;
-    }
-    return sum;
-}
+Example 2:
 
-/// counts all possible digit sums form 1 to num, with sum within min and max
-fn count(num1: i32, num2: i32, min: i32, max: i32) -> i32 {
-    let mut count = 0;
-    for i in num1..num2+1 {
-        let res = digit_sum(i);
-        if res >= min && res <= max {
-            count += 1;
-        }
-    }
-    return  count;
-}
+Input: num1 = "1", num2 = "5", min_sum = 1, max_sum = 5
+Output: 5
+Explanation: The 5 integers whose sum of digits lies between 1 and 5 are 1,2,3,4, and 5. Thus, we return 5.
 
-fn count2(num1: &mut Vec<i32>, num2: Vec<i32>, min: i32, max: i32) -> i32 {
-    if num2.len() > num1.len() {
-        num1.reverse();
-        for _ in num1.len()..num2.len() {
-            num1.push(0);
-        }
-        num1.reverse();
-    }
 
-    let mut out = 0;
-    for i in 0..num2.len() {
-        let diff  = num2[i] - num1[i];
-        let base: i32 = 10;
-        out += diff.abs() * base.pow((num2.len() as u32) - 1 - i as u32);
-    }
-    out += 1;
-    return out;
-}
 
-fn count3(num1: Vec<i32>, num2: Vec<i32>, min: i32, max: i32) -> i32 {
-    let mut working = num1.clone();
-    if num2.len() > working.len() {
-        working.reverse();
-        for _ in working.len()..num2.len() {
-            working.push(0);
-        }
-        working.reverse();
-    }
+Constraints:
 
-    let old_sum = digisum_vect(&working);
-    let count = 0;
-    let mut i = working.len() - 1;
-    while i > 0 {
-        
-        i -= 1;
-    }
+    1 <= num1 <= num2 <= 1022
+    1 <= min_sum <= max_sum <= 400
 
-    return count
-}
-
-fn num_to_vect(num: i32) -> Vec<i32>{
-    let mut outvect: Vec<i32> = Vec::new();
-    let mut working = num;
-    while working > 0 {
-        outvect.push(working % 10);
-        working /= 10;
-    }
-    outvect.reverse();
-    return outvect;
-}
-
-fn digisum_vect(num: &Vec<i32>) -> i32 {
-    let mut out = 0;
-    for n in num {
-        out += n;
-    }
-    return out;
-}
-
+ */
 
 struct Solution;
 
+impl Solution {
+    pub fn count(num1: String, num2: String, min_sum: i32, max_sum: i32) -> i32 {
+        let base = 10;
+
+        let mut count: i128 = 0;
+
+        let n1 = i128::from_str_radix(&num1, base).unwrap(); // intentional panic on parseIntError
+        let n2 = i128::from_str_radix(&num2, base).unwrap(); // intentional panic on parseIntError
+
+        for i in n1..=n2 {
+            let digit_sum = Self::digit_sum(i);
+            if digit_sum >= min_sum && digit_sum <= max_sum {
+                count += 1;
+            }
+        }
+        return (count % 1000000000 + 7) as i32;
+    }
+
+    fn digit_sum(mut num: i128) -> i32 {
+        let mut sum: i32 = 0;
+        while num > 0 {
+            sum += (num % 10) as i32;
+            num /= 10;
+        }
+        sum
+    }
+}
+
 fn main() {
-    let num1vect = vec![1,1,6,4,64,87,332];
-    let num2vect = vec![12,5,23,32,132,124,863];
-    let minsumvect = vec![1,1,8,4,4,7,1];
-    let maxsumvect = vec![12,5,12,8,8,32,400];
+    assert_eq!(Solution::digit_sum(123), 6);
+    assert_eq!(Solution::digit_sum(89), 17);
 
-    // TEST OF STRING DIGIT SUM 
-    // assert_eq!(Solution::digit_sum_str(&"9999999999999999999999".to_string()),  198);
-    // assert_eq!(Solution::digit_sum_str(&"10000000000000000000000".to_string()),  1);
-
-    // TEST OF SOLUTION
-    assert_eq!(Solution::count("1".to_string(), "12".to_string(), 1, 8), 11);
-    assert_eq!(Solution::count("1".to_string(), "5".to_string(), 1, 5), 5);
-    assert_eq!(Solution::count("1000000007".to_string(), "2000000014".to_string(), 1, 400), 1);
-    assert_eq!(Solution::count("332".to_string(), "863".to_string(), 20, 29), 61);
-    assert_eq!(Solution::count("6".to_string(), "23".to_string(), 8, 12), 5);
-    assert_eq!(Solution::count("4".to_string(), "32".to_string(), 4, 8), 17);
-    // assert_eq!(Solution::count("862662375825717340797".to_string(), "1341489267173864014004".to_string(), 204, 338), 0);
-    // assert_eq!(Solution::count("1".to_string(), "10000000000000000000000".to_string(), 1, 400), 1);
-}   
+    assert_eq!(Solution::count("1".into(), "12".into(), 1, 8), 11 + 7);
+    assert_eq!(Solution::count("1".into(), "5".into(), 1, 5), 5 + 7);
+}
